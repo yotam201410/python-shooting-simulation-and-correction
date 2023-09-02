d4e3b95645100b5a5a3632b007be417cc91ba942
yotam <yotam201410@gmail.com>
2023-09-02 03:07:50 +0300

    added docs and changes to gitignore

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ from Constants import Constants
 import time
 import numpy as np
 import math
-dt  = 0
+
+dt = 0
 vis = visualization()
 controller = XboxController()
-def move_robot_pos(velocity:np.array,dt):
-    vis.set_box_position(vis.robot_location+velocity*dt)
-def move_robot_rotation(rotational_velocity:float,dt):
-    vis.set_box_rotation(math.degrees(vis.robot_rotation)+rotational_velocity*dt)
+
+
+def move_robot_pos(velocity: np.ndarray, dt: float):
+    """
+        moves the robot position by using the velocity of the robot
+        :param velocity: the velocity of the robot in meters/second
+        :param dt: the amount of time that passed since the last update
+        """
+    vis.set_box_position(vis.robot_location + velocity * dt)
+
+
+def move_robot_rotation(rotational_velocity: float, dt: float):
+    """
+    moves the robot rotation by using the rotational velocity of the robot
+    :param rotational_velocity: the rotational velocity of the robot in degrees/second
+    :param dt: the amount of time that passed since the last update
+    """
+    vis.set_box_rotation(math.degrees(vis.robot_rotation) + rotational_velocity * dt)
+
 
 while True:
     start = time.time()
     controls = controller.read()
-    move_robot_pos(np.array([-controls[1],controls[0],0])*Constants.max_velocity,dt)
-    move_robot_rotation(controls[2]*Constants.max_rotational_velocity,dt)
+    move_robot_pos(np.array([-controls[1], controls[0], 0]) * Constants.max_velocity, dt)
+    move_robot_rotation(controls[2] * Constants.max_rotational_velocity, dt)
     dt = time.time() - start
-    
